0ed7c5cc5d53571e86cf17738282c9cc14498016
Thiago Batista <thiago_gbcm@hotmail.com>
2023-08-08 23:42:11 -0300

    Update main.js

--- Modulo 7/main.js
+++ Modulo 7/main.js
@@ -1,11 +1,10 @@
 function maior(){
 var numero1 = parseFloat(document.getElementById("numeroA").value);
 var numero2 = parseFloat(document.getElementById("numeroB").value);
-if (numero1 > numero2)
-alert(numero1 + " é maior que " + numero2);
-else
-if(numero2 > numero1)
-   alert(numero2 + " é maior que " + numero1);
-else
-   alert(numero1 + " é igual a " + numero2);
+
+if (numero2 > numero1) {
+   alert(numero2 + " é maior que " + numero1)
+} else {
+   alert("O numero contido em B tem que ser maior que em A")
+}
 }
